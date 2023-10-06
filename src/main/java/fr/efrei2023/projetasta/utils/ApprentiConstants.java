6e2d68f08232191ed25ef816aeadd07875efcf07
soum <hugo.soum-brugevin@efrei.net>
2023-10-06 11:49:46 +0200

    added Apprenti Creation

--- src/main/java/fr/efrei2023/projetasta/utils/ApprentiConstants.java
+++ src/main/java/fr/efrei2023/projetasta/utils/ApprentiConstants.java
@@ -1,4 +1,21 @@
 package fr.efrei2023.projetasta.utils;
 
 public class ApprentiConstants {
+
+
+    // GENERIC FIELD NAMES
+
+    public static final String ID_APPRENTI = "idApprenti";
+    public static final String NUMERO_ETUDIANT = "numeroEtudiant";
+    public static final String PROGRAMME = "programme";
+    public static final String ANNEE_ACADEMIQUE = "anneeAcademique";
+    public static final String MAJEURE = "majeure";
+
+    // JSP PAGES
+
+    public static final String APPRENTI_REGISTER_PAGE = "apprenti-register.jsp";
+    public static final String APPRENTI_LIST_PAGE = "apprenti-list.jsp";
+    public static final String APPRENTI_HOME_PAGE = "apprenti-home.jsp";
+
+
 }
